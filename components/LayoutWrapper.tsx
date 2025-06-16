4bd4d5e5e7084d79394411e301ffe077258934aa
SC-choppa <choppa@supercent.io>
2025-06-17 07:34:59 +0900

    layout

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -3,16 +3,11 @@ import { ReactNode } from "react";
 
 export default function LayoutWrapper({ children }: { children: ReactNode }) {
   const router = useRouter();
-
   return (
-    <div className="min-h-screen bg-white text-black">
+    <div className="min-h-screen bg-white dark:bg-gray-950 text-black dark:text-white">
       <div className="p-4">
-        <button
-          onClick={() => router.push("/")}
-          className="inline-flex items-center space-x-2 hover:opacity-80"
-        >
-          <img src="/arrow-left.svg" alt="Back" className="w-5 h-5" />
-          <span className="sr-only">뒤로가기</span>
+        <button onClick={() => router.push("/")} className="inline-flex items-center space-x-2 hover:opacity-80">
+          <img src="/arrow-left.svg" alt="뒤로가기" className="w-5 h-5" />
         </button>
       </div>
       <div className="p-4">{children}</div>
